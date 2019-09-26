710da74e09d82eb5909f1cbeac2f37ab7cc720f0
MochRizkiMaulanaN <55683135+MochRizkiMaulanaN@users.noreply.github.com>
2019-09-26 14:57:45 +0700

    Add files via upload

---
+++ Integer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package integer;
+
+/**
+ * Nama     : Moch Rizki Maulana N
+ * Kelas    : IF-1 2018
+ * Nim      : 10118012
+ * Deskripsi Program : Integer
+ *
+ * @author LENOVO
+ */
+public class Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        
+        b= 120;
+        s= 32767;
+        i= 1_000_000-000;
+        l= 90000000000L;
+        
+        System.out.println("Byte\t\t: "+b);
+        System.out.println("Short\t\t: "+s);
+        System.out.println("Int\t\t: "+i);
+        System.out.println("Long\t\t: "+l);
+    }
+    
+}
